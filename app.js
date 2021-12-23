f9b3a91055d6c278111f2fbe60920d284828dcf8
Samuel Lucas <samuellucas0603@gmail.com>
2021-12-23 18:22:35 -0300

    feat: adicinando menu de navegação e multiplicação de números

--- app.js
+++ app.js
@@ -2,12 +2,30 @@ var readlineSync = require("readline-sync")
 
 const soma = (a, b) => (a + b).toFixed(2)
 
-const main = async () => {
+const multiplicação = (a, b) => (a * b).toFixed(2)
+
+const somaService = async () => {
   const valueOne = readlineSync.questionFloat("Insira o primeiro valor: ")
   const valueTwo = readlineSync.questionFloat("Insira o segundo valor: ")
 
   const result = soma(parseFloat(valueOne), parseFloat(valueTwo))
   console.log("O resultado da soma é: ", result)
+}
+
+const multiplicaçãoService = async () => {
+  const valueOne = readlineSync.questionFloat("Insira o primeiro valor: ")
+  const valueTwo = readlineSync.questionFloat("Insira o segundo valor: ")
+
+  const result = multiplicação(parseFloat(valueOne), parseFloat(valueTwo))
+  console.log("O resultado da multiplicação é: ", result)
+}
+
+const main = () => {
+  const actions = ["soma", "multiplicação"]
+  const index = readlineSync.keyInSelect(actions, "Escolha uma opção")
+
+  if (actions[index] == "soma") somaService()
+  else if (actions[index] == "multiplicação") multiplicaçãoService()
 }
 
 module.exports = main()
