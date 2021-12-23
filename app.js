b6da287178e72984765cb4e5123730044abceebc
Samuel Lucas <samuellucas0603@gmail.com>
2021-12-23 12:48:01 -0300

    fix: soma de números decimais

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var readlineSync = require("readline-sync")
 
-const soma = (a, b) => a + b
+const soma = (a, b) => (a + b).toFixed(2)
 
 const main = async () => {
   const valueOne = readlineSync.question("Insira o primeiro valor: ")
   const valueTwo = readlineSync.question("Insira o segundo valor: ")
 
-  const result = soma(parseInt(valueOne), parseInt(valueTwo))
+  const result = soma(parseFloat(valueOne), parseFloat(valueTwo))
   console.log("O resultado da soma é: ", result)
 }
 
