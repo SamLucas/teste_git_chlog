ccba3562757cf3d4e569d4a57ecacb7b66cfa2c5
Samuel Lucas <samuellucas0603@gmail.com>
2021-12-23 13:01:29 -0300

    fix: validando os valores de inserção

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var readlineSync = require("readline-sync")
 const soma = (a, b) => (a + b).toFixed(2)
 
 const main = async () => {
-  const valueOne = readlineSync.question("Insira o primeiro valor: ")
-  const valueTwo = readlineSync.question("Insira o segundo valor: ")
+  const valueOne = readlineSync.questionFloat("Insira o primeiro valor: ")
+  const valueTwo = readlineSync.questionFloat("Insira o segundo valor: ")
 
   const result = soma(parseFloat(valueOne), parseFloat(valueTwo))
   console.log("O resultado da soma é: ", result)
