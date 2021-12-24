cc03f74524de99b1bda9db706e6de14fd93e8fe6
Samuel Lucas <samuellucas0603@gmail.com>
2021-12-24 10:14:02 -0300

    feat: menu de navegação

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ var readlineSync = require("readline-sync")
 
 const soma = (a, b) => (a + b).toFixed(2)
 
-const main = async () => {
+const somaService = async () => {
   const valueOne = readlineSync.questionFloat("Insira o primeiro valor: ")
   const valueTwo = readlineSync.questionFloat("Insira o segundo valor: ")
 
   const result = soma(parseFloat(valueOne), parseFloat(valueTwo))
   console.log("O resultado da soma é: ", result)
+}
+
+const main = () => {
+  const actions = ["soma"]
+  const index = readlineSync.keyInSelect(actions, "Escolha uma opção")
+
+  if (actions[index] == "soma") somaService()
 }
 
 module.exports = main()
