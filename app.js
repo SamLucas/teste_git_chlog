fbd26055de864944c6b7fe7166fe96f04e22d6d6
Samuel Lucas <samuellucas0603@gmail.com>
2021-12-24 10:23:28 -0300

    feat: multiplicação

--- app.js
+++ app.js
@@ -10,11 +10,22 @@ const somaService = async () => {
   console.log("O resultado da soma é: ", result)
 }
 
+const multiplicação = (a, b) => (a * b).toFixed(2)
+
+const multiplicaçãoService = async () => {
+  const valueOne = readlineSync.questionFloat("Insira o primeiro valor: ")
+  const valueTwo = readlineSync.questionFloat("Insira o segundo valor: ")
+
+  const result = multiplicação(parseFloat(valueOne), parseFloat(valueTwo))
+  console.log("O resultado da multiplicação é: ", result)
+}
+
 const main = () => {
-  const actions = ["soma"]
+  const actions = ["soma", "multiplicação"]
   const index = readlineSync.keyInSelect(actions, "Escolha uma opção")
 
   if (actions[index] == "soma") somaService()
+  else if (actions[index] == "multiplicação") multiplicaçãoService()
 }
 
 module.exports = main()
