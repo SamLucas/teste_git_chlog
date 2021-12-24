2374e12ebf4c17734e3823689a1a3ec28d88f594
Samuel Lucas <samuellucas0603@gmail.com>
2021-12-24 08:58:51 -0300

    feat: adicionando divisao

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const soma = (a, b) => (a + b).toFixed(2)
 
 const multiplicação = (a, b) => (a * b).toFixed(2)
 
+const divisao = (a, b) => (a / b).toFixed(2)
+
 const somaService = async () => {
   const valueOne = readlineSync.questionFloat("Insira o primeiro valor: ")
   const valueTwo = readlineSync.questionFloat("Insira o segundo valor: ")
@@ -20,12 +22,21 @@ const multiplicaçãoService = async () => {
   console.log("O resultado da multiplicação é: ", result)
 }
 
+const divisaoService = async () => {
+  const valueOne = readlineSync.questionFloat("Insira o primeiro valor: ")
+  const valueTwo = readlineSync.questionFloat("Insira o segundo valor: ")
+
+  const result = divisao(parseFloat(valueOne), parseFloat(valueTwo))
+  console.log("O resultado da divisão é: ", result)
+}
+
 const main = () => {
-  const actions = ["soma", "multiplicação"]
+  const actions = ["soma", "multiplicação", "divisão"]
   const index = readlineSync.keyInSelect(actions, "Escolha uma opção")
 
   if (actions[index] == "soma") somaService()
   else if (actions[index] == "multiplicação") multiplicaçãoService()
+  else if (actions[index] == "divisão") divisaoService()
 }
 
 module.exports = main()
