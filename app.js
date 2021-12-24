cbbcf161a83cd41a643293c9d5a53cce5415e114
Samuel Lucas <samuellucas0603@gmail.com>
2021-12-24 10:45:57 -0300

    feat: divisão

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var readlineSync = require("readline-sync")
 
+// Soma
+
 const soma = (a, b) => (a + b).toFixed(2)
 
 const somaService = async () => {
@@ -9,6 +11,8 @@ const somaService = async () => {
   const result = soma(parseFloat(valueOne), parseFloat(valueTwo))
   console.log("O resultado da soma é: ", result)
 }
+
+// Multiplicação
 
 const multiplicação = (a, b) => (a * b).toFixed(2)
 
@@ -20,12 +24,25 @@ const multiplicaçãoService = async () => {
   console.log("O resultado da multiplicação é: ", result)
 }
 
+// Divisão
+
+const divisao = (a, b) => (a / b).toFixed(2)
+
+const divisaoService = async () => {
+  const valueOne = readlineSync.questionFloat("Insira o primeiro valor: ")
+  const valueTwo = readlineSync.questionFloat("Insira o segundo valor: ")
+
+  const result = divisao(parseFloat(valueOne), parseFloat(valueTwo))
+  console.log("O resultado da divisão é: ", result)
+}
+
 const main = () => {
-  const actions = ["soma", "multiplicação"]
+  const actions = ["soma", "multiplicação", "divisão"]
   const index = readlineSync.keyInSelect(actions, "Escolha uma opção")
 
   if (actions[index] == "soma") somaService()
   else if (actions[index] == "multiplicação") multiplicaçãoService()
+  else if (actions[index] == "divisão") divisaoService()
 }
 
 module.exports = main()
